6a42f00aa3c90bd42f3882ebc57f42c6195e10b6
nagpaleshant1 <nagpaleshant1@gmail.com>
2018-06-01 23:59:59 +0530

    Create murder_quiz.js

---
+++ murder_quiz.js
@@ -0,0 +1,26 @@
+var room = "dining room";
+var suspect = "Mr. Parkes";
+
+var weapon= "";
+var solved = false;
+
+if (room === "ballroom" && suspect==="Mr. Kalehoff") {
+    weapon="poison";
+    solved=true;
+
+} else if  (room === "gallery" && suspect==="Ms. Van Cleve") {
+    weapon="trophy";
+    solved = true;
+    
+} else if(room === "billiards room" && suspect==="Mrs. Sparr") {
+    weapon="pool stick";
+    solved=true;
+    
+} else if (room === "dining room" && suspect==="Mr. Parkes") {
+    weapon="knife";
+    solved=true;
+}
+
+if (solved) {
+	console.log(suspect +" did it in the "+room+" with the " + weapon +"!");
+}
