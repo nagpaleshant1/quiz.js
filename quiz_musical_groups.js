cbf6e1a3e94d3f992f6446dd97be1955c878fa4b
nagpaleshant1 <nagpaleshant1@gmail.com>
2018-06-02 00:02:20 +0530

    Create quiz_musical_groups.js

---
+++ quiz_musical_groups.js
@@ -0,0 +1,31 @@
+/*
+ * Programming Quiz: Musical Groups (3-3)
+ */
+
+// change the value of `musicians` to test your conditional statements
+var musicians = 4;
+
+if(musicians === 1){
+    console.log("solo");
+}
+
+else if(musicians === 2){
+    console.log("duet");
+}
+
+else if(musicians === 3){
+    console.log("trio");
+}
+
+else if(musicians === 4){
+    console.log("quartet");
+}
+
+else if(musicians > 4)
+{
+    console.log("this is a large group");
+}
+else {
+    console.log("not a group");
+}
+// your code goes here
